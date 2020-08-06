b05c3f2d97c43524459f7e72fa364b66644ad2ba
Esha Bobde <Esha Bobde@LAPTOP-2FEG0L62.home>
2020-08-06 07:05:32 -0400

    If Else statements

---
+++ AndroidApp/src/main/java/ImportScanner/IfElse.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ImportScanner;
+
+/**
+ *
+ * @author Esha Bobde
+ */
+public class IfElse {
+    public static void main(String []arg) {
+        int number = 11;
+        if (number > 56) {  
+                System.out.println("The number is positive.");
+            }
+        else
+        {      
+            System.out.println("The number is negative.");
+        }
+    }
+}
